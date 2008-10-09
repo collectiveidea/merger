d0cc887733f404a592ca8ff3b107139e235ea240
Daniel Morrison <daniel@collectiveidea.com>
2008-10-09 10:36:29 -0400

    Small refactor of merge!

--- lib/merger.rb
+++ lib/merger.rb
@@ -7,7 +7,7 @@ module Merger
   
   module InstanceMethods
     def merge!(*others)
-      Merger::Merge.new(others.push(self)).merge!
+      Merger::Merge.new(others, :keep => self).merge!
     end
   end
 end
